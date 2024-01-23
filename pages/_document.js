2360b7451bc677c933e9439f66bf89833242b5fc
gaurav bharti <gauravpixxelu@gmail.com>
2024-01-23 16:09:37 +0530

    next update

--- pages/_document.js
+++ pages/_document.js
@@ -1,20 +1,25 @@
 import Footer from '@/components/layout/footer'
 import Header from '@/components/layout/header'
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-      <link rel="shortcut icon" href="../favicon.svg" />
-      </Head>
-      <body>
-        <Header />
-        <Main />
-        <NextScript />
-        <Footer />
-      </body>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="shortcut icon" href="../favicon.svg" />
+        </Head>
+        <body>
+          <Header />
+          <Main />
+          <NextScript />
+          <Footer />
+          <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+            defer
+          ></script>
+        </body>
+      </Html>
+    );
+  }
 }
